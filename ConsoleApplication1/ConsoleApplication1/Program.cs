364dde7dbfb848e848413d84191fb63949ff3960
Amadeusz Wieczorek <git@amadeusw.com>
2016-09-21 17:55:07 -0700

    emits!

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
 
 namespace ConsoleApplication1
 {
@@ -10,7 +12,33 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            var solutionPath = @"C:\git\roslyn\Roslyn.sln";
+            var outputDirectory = @"D:\output";
 
+            var workspace = Microsoft.CodeAnalysis.MSBuild.MSBuildWorkspace.Create();
+            Console.WriteLine("Loading solution");
+            var solution = workspace.OpenSolutionAsync(solutionPath).Result;
+            var project = solution.Projects.First(n => n.Name.Equals("CSharpCompilerSemanticTest"));
+            Console.WriteLine("Getting compilation");
+            var compilation = project.GetCompilationAsync().Result;
+
+            Console.WriteLine("Emission");
+            var assemblyOutputPath = Path.Combine(outputDirectory, project.AssemblyName + ".dll");
+            var pdbOutputPath = Path.Combine(outputDirectory, project.AssemblyName + ".pdb");
+            var result = compilation.Emit(assemblyOutputPath, pdbOutputPath);
+            if (!result.Success)
+            {
+                foreach (var err in result.Diagnostics.Where(n => n.Severity >= DiagnosticSeverity.Error))
+                {
+                    Console.WriteLine(err);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Success");
+            }
+
+            Console.ReadLine();
         }
     }
 }
